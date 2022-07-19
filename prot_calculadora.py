f2f4eb9016be88f7846ee7c4fdf342786ad1a152
luisr-1 <99516696+luisr-1@users.noreply.github.com>
2022-07-19 18:00:43 -0300

    Adicionado novas funcionalidades e algumas correções de bug.

--- prot_calculadora.py
+++ prot_calculadora.py
@@ -1,39 +1,130 @@
-def mainCalculator():
-    
-    operadores = ["+","-","/","*","^"] #Array contendo operações básicas
-    entrada = 0 #Inicializando as variáveis.
+from math import log, pi, cos, tan, sin, e
+
+
+def menu():
+    print("Selecione uma opção:\n[1]: Operações matemáticas  "
+          "       [2]: Conversão de unidades          "
+          "[3]: Gráficos        [4]: Desligar ")
+
+    entrada_opcao = int(input())
+
+    if entrada_opcao == 1:
+        return operations(potencia, soma, subtracao, multiplicacao, divisao, logaritmo, raiz)
+
+    # elif entrada_opcao == 2:
+    #   return conversao_unidades()
+
+    # elif entrada_opcao == 3:
+    #   return graficos()
+
+    # elif entrada_opcao == 4:
+    #   return desligar()
+
+
+def operations(potencia, soma, subtracao, multiplicacao, divisao, logaritmo, raiz):
+    operadores = ["+", "-", "/", "*", "^", "log", "raiz"]
+    entrada = 0
     conclusao = 0
-    
+    numero = 0
+    numero2 = 0
 
-    while entrada != str(""): #Enquanto a entrada for diferente de um espaço branco continuará pedindo informação para as variáveis
-        numero = float(input()) #Recebe um número do teclado
-        entrada = str(input()) #Essa entrada é do operador
-        numero2 = float(input()) #Aqui é o segundo número que o usuário irá colocar
-        conclusao = input() 
-        if entrada == operadores[4] and conclusao == "": #Caso a entrada seja igual ao operador ^, então realiza a operação de potência.
-            resultado = numero ** numero2 #O cálculo é realizado
-            print(f"{numero}{entrada}{numero2}={resultado}") #É impresso na tela
-        
-        elif entrada == operadores[2] and conclusao == "": #Como multiplicação e divisão tem mesma precedência foi colocado em uma mesma condição
-            if entrada == operadores[2]: #Caso a entrada seja a operação de divisão
-                try: #Tenta a divisão
-                    resultado = numero / numero2
-                    print(f"{numero}{entrada}{numero2}={resultado}")
-                except: #Caso o numero2 seja igual a zero, aparece a mensagem de erro de sytax e pede para o usuário fazer outra operação 
-                    if numero2 == 0:
-                        print("Sytax error: The division by 0 is not defined\nPlease try another operation again!")
-                        mainCalculator() #Chama o método principal para recomeçar o código.
-                
-        elif entrada == operadores[3] and conclusao == '': #Caso a entrada seja igual a multiplicação então roda o código abaixo
-            resultado = numero * numero2 #Multiplicação não possui restrições, então roda normal
-            print(f"{numero}{entrada}{numero2}={resultado}") #Imprime a operação na tela
+    while conclusao != str(""):
+        numero = float(input())
+        print(f"{numero}")
+        entrada = str(input())
+        print(f"{numero} {entrada}")
+        if entrada == "":
+            print("Entrada esperada símbolo aritmético")
+            while entrada not in operadores:
+                entrada = input()
 
-        elif entrada == operadores[0] and conclusao == '':#Se a entrada for igual a adição então o código abaixo irá rodar
-            resultado = numero + numero2
-            print(f"{numero}{entrada}{numero2}={resultado}")
+        numero2 = float(input())
+        print(f"{numero} {entrada} {numero2}")
 
-        elif entrada == operadores[1] and conclusao == '':
-            resultado = numero - numero2
-            print(f"{numero}{entrada}{numero2}={resultado}")
+        while conclusao != "":
+            print("Pressione a tecla ENTER para computar o valor")
+            conclusao = input()
 
-mainCalculator() #chamada do método principal
+    if entrada == operadores[0] and conclusao == "":
+        return soma(entrada, numero, numero2)
+
+    elif entrada == operadores[1] and conclusao == "":
+        return subtracao(entrada, numero, numero2)
+
+    elif entrada == operadores[2] and conclusao == "":
+        return divisao(entrada, numero, numero2)
+
+    elif entrada == operadores[3] and conclusao == "":
+        return multiplicacao(entrada, numero, numero2)
+
+    elif entrada == operadores[4] and conclusao == "":
+        return potencia(entrada, numero, numero2)
+
+    elif entrada == operadores[5] and conclusao == "":
+        return logaritmo(entrada, numero, numero2)
+
+    elif entrada == operadores[6] and conclusao == "":
+        return raiz(entrada, numero, numero2)
+
+
+def soma(entrada, numero, numero2):
+    resultado = numero + numero2
+    print(f"{numero} {entrada} {numero2} = {resultado}")
+    return menu()
+
+
+def subtracao(entrada, numero, numero2):
+    resultado = numero - numero2
+    print(f"{numero} {entrada} {numero2} = {resultado}")
+    return menu()
+
+
+def multiplicacao(entrada, numero, numero2):
+    resultado = numero * numero2
+    print(f"{numero} {entrada} {numero2} = {resultado}")
+    return menu()
+
+
+def divisao(entrada, numero, numero2):
+    try:
+        resultado = numero / numero2
+        print(f"{numero} {entrada} {numero2} = {resultado}")
+        return menu()
+
+    except numero2 == 0:
+        print("Erro de sintaxe, a operação de divisão por 0 não é válida\n"
+              "Por favor tente outra operação!")
+        return divisao(entrada, numero, numero2)
+
+
+def potencia(entrada, numero, numero2):
+    resultado = numero ** numero2
+    print(f" {numero} {entrada} {numero2} = {resultado}")
+    return menu()
+
+
+def raiz(entrada, numero, numero2):
+    try:
+        resultado = numero ** (1 / numero2)
+        print(f"Raiz {numero2} de {numero} = {resultado}")
+        return menu()
+
+    except numero < 0 or numero2 == 0:
+        print("Erro de sintaxe, a operação não é válida\n"
+              "Por favor tente outra operação!")
+        return raiz(entrada, numero, numero2)
+
+
+def logaritmo(entrada, numero, numero2):
+    try:
+        resultado = log(numero, numero2)
+        print(f"{entrada} de ({numero},{numero2})={resultado}")
+        return menu()
+
+    except (numero != 1 and numero > 0 or numero2 != 0):
+        print("Erro de sintaxe, a operação não é válida\n"
+              "Por favor tente outra operação!")
+        return logaritmo(entrada, numero, numero2)
+
+
+menu()
